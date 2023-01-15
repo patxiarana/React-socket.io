8cbf30778e9d233569e63e59a930c64e66227406
patxi <patxiarana05@gmail.com>
2023-01-15 11:33:56 -0300

    start npx react

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from 'express'
+import morgan from 'morgan'
+import { Server as SocketServer} from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+import { PORT } from './config.js'
+
 
 const app = express()
 
-app.listen()
-console.log('server on port 3000')
+const server = http.createServer(app)
+const io = new SocketServer(server)
+
+app.use(cors())
+app.use(morgan("dev"))
+
+
+app.listen(PORT)
+console.log('server on port', PORT)
