17338d8810455142fc16fc1e52d21ff7eba73bae
patxi <patxiarana05@gmail.com>
2023-01-15 13:19:41 -0300

    chat terminado, falta estylar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,51 @@
 import './App.css';
+import io from "socket.io-client"
+import {useState, useEffect} from 'react'
+import e from 'cors';
+
+const socket = io('http://localhost:4000/')
 
 function App() {
+
+const [mensaje , setMensaje] = useState('')
+const [mensajes, setMensajes] = useState([])
+
+
+const handelSubmit = (e) => {
+  e.preventDefault()
+  socket.emit('mensaje', mensaje)
+  const nuevoMensaje = {
+    body:mensaje,
+    from:"tu",
+  }
+  setMensajes([ nuevoMensaje, ...mensajes])
+  setMensaje('')
+}
+
+useEffect(() => {
+  const recibirMensaje = mensaje => {
+    setMensajes([mensaje, ...mensajes])
+  }
+  socket.on('mensaje', recibirMensaje )
+
+  return () => {
+    socket.off('mensaje', recibirMensaje )
+  }
+
+}, [mensajes])
+
   return (
     <div className="App">
-     <h1>Hello Wolrd</h1> 
+      <form onSubmit={handelSubmit}>
+     <input type="text" onChange={e => setMensaje(e.target.value)}
+     value={mensaje}/>
+     <button>Send</button>
+     </form>
+     {mensajes.map((mensaje, index) =>(
+      <div key={index}>
+        <p>{mensaje.from}: {mensaje.body}</p>
+      </div>
+     ))}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -9,11 +9,25 @@ import { PORT } from './config.js'
 const app = express()
 
 const server = http.createServer(app)
-const io = new SocketServer(server)
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }
+})
+
+io.on('connection', (socket)=> {
+    console.log(socket.id)
+
+     socket.on('mensaje', (mensaje) =>{
+     console.log(mensaje)
+    socket.broadcast.emit('mensaje',{from:socket.id, body:mensaje}
+    )
+})
+})
 
 app.use(cors())
 app.use(morgan("dev"))
 
 
-app.listen(PORT)
+server.listen(PORT)
 console.log('server on port', PORT)
